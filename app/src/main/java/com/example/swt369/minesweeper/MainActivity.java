181b8eb92c7787af31b23d7a668443af4161f19a
swt369 <343011953@qq.com>
2017-07-19 11:22:23 +0800

    add highscore

--- app/src/main/java/com/example/swt369/minesweeper/MainActivity.java
+++ app/src/main/java/com/example/swt369/minesweeper/MainActivity.java
@@ -4,11 +4,13 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
 import android.os.Message;
 import android.support.annotation.IdRes;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -18,6 +20,14 @@ import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
     private final Brick[][][] bricks = new Brick[1][][];
     private Timer timer;
@@ -26,10 +36,14 @@ public class MainActivity extends AppCompatActivity {
     private Handler handler;
     private GameView gameView;
     private GameController gameController;
+    private File destDir;
+    private File fileTxt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initializeHighScore();
 
         handler = new Handler(new Handler.Callback() {
             @Override
@@ -220,10 +234,117 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void createDialogForWin(){
+        int[] lastHighScores = getHighScore();
+        int lastHighScore = 999;
+        switch (radioGroup.getCheckedRadioButtonId() % 3){
+            case 1:
+                lastHighScore = lastHighScores[0];
+                break;
+            case 2:
+                lastHighScore = lastHighScores[1];
+                break;
+            case 0:
+                lastHighScore = lastHighScores[2];
+        }
         AlertDialog builder = new AlertDialog.Builder(this).create();
         builder.setTitle("你赢了！");
-        builder.setMessage(String.format("%s  用时%d秒",getDifficultyText(),timer.getTime()));
+        if(timer.getTime() < lastHighScore){
+            builder.setMessage(String.format("%s新纪录!\n用时:   %d秒\n原纪录： %d秒",getDifficultyText(),timer.getTime(),lastHighScore));
+            refreshHighScore(timer.getTime());
+        }else {
+            builder.setMessage(String.format("用时:   %d秒\n原纪录： %d秒",timer.getTime(),lastHighScore));
+        }
         builder.show();
+    }
+
+    private void initializeHighScore(){
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT) {
+            destDir = new File(this.getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS).getPath());
+        }else {
+            destDir = new File(this.getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS).getPath());
+        }
+        if (!destDir.exists()) {
+            destDir.mkdirs();
+            Log.i("created","dir");
+        }
+        fileTxt = new File(destDir.getPath() + File.separator + "highscore.txt");
+        if(!fileTxt.exists()){
+            BufferedWriter bufferedWriter = null;
+            try {
+                bufferedWriter = new BufferedWriter(new FileWriter(fileTxt));
+                bufferedWriter.write("999\n999\n999");
+            }catch (Exception e){
+                e.printStackTrace();
+            }finally {
+                try {
+                    if (bufferedWriter != null) {
+                        bufferedWriter.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private int[] getHighScore(){
+        if(!fileTxt.exists() || !destDir.exists()){
+            initializeHighScore();
+        }
+        int[] highScores = new int[]{999,999,999};
+        BufferedReader bufferedReader = null;
+        try {
+            bufferedReader = new BufferedReader(new FileReader(fileTxt));
+            for(int i = 0 ; i < 3 ; i++){
+                highScores[i] = Integer.parseInt(bufferedReader.readLine());
+            }
+            return highScores;
+        } catch (Exception e) {
+            return new int[]{999,999,999};
+        } finally {
+            try {
+                if (bufferedReader != null) {
+                    bufferedReader.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void refreshHighScore(int highScore){
+        BufferedWriter bufferedWriter = null;
+        String res;
+        try {
+            int[] highScores = getHighScore();
+            switch (radioGroup.getCheckedRadioButtonId() % 3){
+                case 1:
+                    highScores[0] = highScore;
+                    break;
+                case 2:
+                    highScores[1] = highScore;
+                    break;
+                case 0:
+                    highScores[2] = highScore;
+                    break;
+            }
+            res = String.format("%d\n%d\n%d",highScores[0],highScores[1],highScores[2]);
+        } catch (Exception e) {
+            return;
+        }
+        try {
+            bufferedWriter = new BufferedWriter(new FileWriter(fileTxt));
+            bufferedWriter.write(res);
+        } catch (IOException ignored) {
+        }finally {
+            try {
+                if (bufferedWriter != null) {
+                    bufferedWriter.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     private static class Settings{
